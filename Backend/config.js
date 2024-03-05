6b9da02428921f1c812c6c382cc8f4f1addf98df
Emran147 <emran.qedan11@hotmail.com>
2024-03-05 20:40:45 +0200

    added more checks and messages for the login and signup

---
+++ Backend/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    errorMessages: {
+        invalidEmailFormat: "Invalid email format",
+        emailAlreadyExists: "Email already exists",
+        phoneNumberAlreadyExists: "Phone number already exists",
+        phoneNumberTooShort: "Phone number is too short",
+        phoneNumberTooLong: "Phone number is too long",
+        passwordTooShort: "Password must be at least 8 characters long",
+        passwordNoSpecialChar: "Password must contain at least one special character (!@#$%^&*(),.?\":{}|<>)",
+        emailNotFound: "Email not found",
+        wrongPassword: "Wrong password",
+        internalServerError: "Internal server error"
+    }
+};
