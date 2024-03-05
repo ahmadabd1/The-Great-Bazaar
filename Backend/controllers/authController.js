9ade22099560bd8225825da496ab511ebc2fadb7
Emran147 <emran.qedan11@hotmail.com>
2024-03-05 20:15:47 +0200

    added the user login and register

--- Backend/controllers/authController.js
+++
@@ -1,8 +0,0 @@
-
-module.exports.signup_post = (req,res)=>{
-    res.send('new signup')
-}
-
-module.exports.signin_get = (req,res)=>{
-    res.send('sign in succes')
-}
--- Backend/server.js
+++ Backend/server.js
@@ -1,21 +1,22 @@
-const express = require('express')
-const app = express()
-const api = require('./routes/api')
-const path = require('path')
+const express = require('express');
+const app = express();
+const authRoutes = require('./routes/authRoutes'); // Import authRoutes.js
+const path = require('path');
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 const mongoose = require('mongoose');
-mongoose.connect("mongodb://127.0.0.1:27017/TheGreateBazar" , { useNewUrlParser: true })
+mongoose.connect("mongodb://127.0.0.1:27017/TheGreateBazar", { useNewUrlParser: true });
 
 app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    next();
+});
 
-    next()
-})
-app.use('/', api)
-const PORT = 8080
-app.listen(process.env.PORT || PORT)
+app.use('/', authRoutes); // Mount authRoutes
+
+const PORT = 8080;
+app.listen(process.env.PORT || PORT);
