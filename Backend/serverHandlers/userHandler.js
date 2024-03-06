d105ed9ff3af0dcdb6174d5fa71963bcf0f8ca6f
lyaleaz <lyale000az@gmail.com>
2024-03-06 13:42:07 +0200

    log in and signup page

--- Backend/serverHandlers/userHandler.js
+++ Backend/serverHandlers/userHandler.js
@@ -1,44 +1,42 @@
-const User = require('../models/user');
+const User = require("../models/user");
 
 exports.signup = async (req, res) => {
-    try {
-        const { email, password, phoneNumber } = req.body;
-        const existingEmail = await User.findOne({ email })
-        if (existingEmail) {
-            return res.status(400).json({ message: "Email already exists" })
-        }
-        const existingPhoneNumber = await User.findOne({ phoneNumber });
-        if (existingPhoneNumber) {
-            return res.status(400).json({ message: "Phone number already exists" })
-        }
-        const newUser = new User({
-            email,
-            password,
-            phoneNumber
-        });
-        await newUser.save()
-        res.status(201).json({ message: "User created successfully" })
-    } catch (error) {
-        console.error("Error creating user:", error)
-        res.status(500).json({ message: "Internal server error" })
+  try {
+    const { email, password, phoneNumber } = req.body;
+    const existingEmail = await User.findOne({ email });
+    if (existingEmail) {
+      return res.status(400).send("email already exsits!");
     }
-}
-
+    const existingPhoneNumber = await User.findOne({ phoneNumber });
+    if (existingPhoneNumber) {
+      return res.status(400).json({ message: "Phone number already exists" });
+    }
+    const newUser = new User({
+      email,
+      password,
+      phoneNumber,
+    });
+    await newUser.save();
+    res.status(201).json({ message: "User created successfully" });
+  } catch (error) {
+    console.error("Error creating user:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
 
 exports.login = async (req, res) => {
-    try {
-        const { email, password } = req.body
-        const user = await User.findOne({ email })
-        if (!user) {
-            return res.status(401).json({ message: "Email not found" })
-        }
-        if (user.password !== password) {
-            return res.status(401).json({ message: "Wrong password" })
-        }
-        res.status(200).json({ message: "Login successful" })
-    } catch (error) {
-        console.error("Error logging in:", error)
-        res.status(500).json({ message: "Internal server error" })
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({ message: "Email not found" });
     }
-}
-
+    if (user.password !== password) {
+      return res.status(401).json({ message: "Wrong password" });
+    }
+    res.status(200).json({ message: "Login successful" });
+  } catch (error) {
+    console.error("Error logging in:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
