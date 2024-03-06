c95c5a3c8cf3d00e18646b70e31e63ef45d99a3a
Ahmad Abdallah <ahmadabdallah174@gmail.com>
2024-03-06 16:03:03 +0200

    first step to build to options for admin and client

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,9 +1,36 @@
 import "./App.css";
 import LoginPage from "../components/LoginPage";
+import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SideBar from "../components/admin/SideBar.jsx";
+
+
+const admin =true
+
 function App() {
   return (
     <>
-      <LoginPage />
+      {admin ? (
+        <Router>
+          {/* // <Route path="/" element={<SideBar />} /> */}
+          <SideBar />
+          {/* <Routes>
+
+          </Routes> */}
+
+        </Router>
+      ) : (
+        <Router>
+          <Navbar balance={balance} />
+          <main className="main-content">
+            <Routes>
+              <Route path="/login" element={<LoginPage />} />
+            
+            </Routes>
+          </main>
+          <Footer />
+        </Router>
+      )}
     </>
   );
 }
