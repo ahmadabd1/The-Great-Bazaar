5bd1c16949330e3e7f6a35111f23c8c78395a6aa
Ahmad Abdallah <ahmadabdallah174@gmail.com>
2024-03-06 21:04:41 +0200

    bug in the loginPage.jsx

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -3,36 +3,49 @@ import LoginPage from "../components/LoginPage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SideBar from "../components/admin/SideBar.jsx";
 import HomaPage from "../components/client/HomePage.jsx"
+import AdminMainPage from '../components/admin/AdminMainPage';
+import ClientMainPage from '../components/client/ClientMainPage';
+
 
 const admin =false
 
 function App() {
   return (
-    <>
-      {admin ? (
-        <Router>
-          {/* // <Route path="/" element={<SideBar />} /> */}
-          <SideBar />
-          {/* <Routes>
+    <div className="App">
+    <Router>
+      <Routes>
+        <Route path='/' element={<LoginPage />} />
+        <Route path='/adminMainPage' element={<AdminMainPage />} />
+        <Route path='/ClientMainPage' element={<ClientMainPage />} />
+      </Routes>
+    </Router>
+  </div>
 
-          </Routes> */}
-
-        </Router>
-      ) : (
-        <Router>
-        {/*   <Navbar balance={balance} /> */}
-          <main className="main-content">
-            <Routes>
-              <Route path="/login" element={<LoginPage />} />
-                    <Route path="/home" element={<HomaPage/>} />
-
-            </Routes>
-          </main>
-        {/*   <Footer /> */}
-        </Router>
-      )}
-    </>
   );
 }
 
 export default App;
+
+
+// {admin ? (
+//   <Router>
+//     {/* // <Route path="/" element={<SideBar />} /> */}
+//     <SideBar />
+//     {/* <Routes>
+
+//     </Routes> */}
+
+//   </Router>
+// ) : (
+//   <Router>
+//   {/*   <Navbar balance={balance} /> */}
+//     <main className="main-content">
+//       <Routes>
+//         <Route path="/login" element={<LoginPage />} />
+//               <Route path="/home" element={<HomaPage/>} />
+
+//       </Routes>
+//     </main>
+//   {/*   <Footer /> */}
+//   </Router>
+// )}
