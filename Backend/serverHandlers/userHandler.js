532de4a5fe1a80d2d9f4774b94115701c4eba3f5
Emran147 <emran.qedan11@hotmail.com>
2024-03-06 18:18:45 +0200

    some backend cleaning

--- Backend/serverHandlers/userHandler.js
+++ Backend/serverHandlers/userHandler.js
@@ -1,20 +1,20 @@
 const User = require("../models/user");
+const { errorMessages } = require("../config");
 
 exports.signup = async (req, res) => {
   try {
     const { email, password, phoneNumber } = req.body;
-     // Check if email format is valid
      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-     if (!emailRegex.test(email) && email!= "admin") {
-       return res.status(400).json({ message: "Invalid email format" });
+     if (!emailRegex.test(email) && email !== "admin") {
+       return res.status(400).json({ message: errorMessages.invalidEmailFormat });
      }
     const existingEmail = await User.findOne({ email });
     if (existingEmail) {
-      return res.status(400).send("Email already exists");
+      return res.status(400).send(errorMessages.emailAlreadyExists);
     }
     const existingPhoneNumber = await User.findOne({ phoneNumber });
     if (existingPhoneNumber) {
-      return res.status(400).json({ message: "Phone number already exists" });
+      return res.status(400).json({ message: errorMessages.phoneNumberAlreadyExists });
     }
     const newUser = new User({
       email,
@@ -25,7 +25,7 @@ exports.signup = async (req, res) => {
     res.status(201).json({ message: "User created successfully" });
   } catch (error) {
     console.error("Error creating user:", error);
-    res.status(500).json({ message: "Internal server error" });
+    res.status(500).json({ message: errorMessages.internalServerError });
   }
 };
 
@@ -34,14 +34,14 @@ exports.login = async (req, res) => {
     const { email, password } = req.body;
     const user = await User.findOne({ email });
     if (!user) {
-      return res.status(401).json({ message: "Email not found" });
+      return res.status(401).json({ message: errorMessages.emailNotFound });
     }
     if (user.password !== password) {
-      return res.status(401).json({ message: "Wrong password" });
+      return res.status(401).json({ message: errorMessages.wrongPassword });
     }
     res.status(200).json({ message: "Login successful" });
   } catch (error) {
     console.error("Error logging in:", error);
-    res.status(500).json({ message: "Internal server error" });
+    res.status(500).json({ message: errorMessages.internalServerError });
   }
 };
