ce068eeede8b724f2dc8d068ab537a07ed5fc0ad
Emran147 <emran.qedan11@hotmail.com>
2024-03-08 22:19:16 +0200

    ezdhar

--- FrontEnd/src/components/admin/AdminMainPage.jsx
+++ FrontEnd/src/components/admin/AdminMainPage.jsx
@@ -1,11 +1,4 @@
-
-
 export default function AdminMainPage() {
- /* const logout = () => {
-   
-    localStorage.removeItem('token');
-
- window.location.href = '/login';  }; */
   return (
     <div>
         Admin Main Page     
--- FrontEnd/src/components/outside/LoginPage.jsx
+++ FrontEnd/src/components/outside/LoginPage.jsx
@@ -1,174 +1,108 @@
-import { useState } from "react";
-import "../style/LoginPage.css";
+import usePost from '../customHooks/usePost';
 import { useNavigate } from 'react-router-dom';
-const LoginPage = () => {
-  const [showSignup, setShowSignup] = useState(false);
-  const [showLogin, setShowLogin] = useState(false);
-  const [message, setMessage] = useState("");
-   const navigate = useNavigate();
-  const [signUpData, setSignUpData] = useState({
-    email: "",
-    password: "",
-    confirmPassword: "",
-    phoneNumber: "",
-  })
-  const [signInData, setSignInData] = useState({
-    email: "",
-    password: "",
-  })
-  const [userType, setUserType] = useState(null);
+import { useState } from 'react';
+import '../style/LoginPage.css'
+const LoginPage = ({ handleUserType }) => {
+  const navigate = useNavigate();
+  const [formType, setFormType] = useState('login'); // 'login' or 'signup'
+  const [message, setMessage] = useState('');
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+    confirmPassword: '',
+    phoneNumber: '',
+  });
+
+  const { postData } = usePost();
 
   const handleChange = (event) => {
     const { name, value } = event.target;
-    if (showLogin) {
-      setSignInData((prevData) => ({
-        ...prevData,
-        [name]: value,
-      }));
-    } else {
-      setSignUpData((prevData) => ({
-        ...prevData,
-        [name]: value,
-      }));
-    }
-  };
-  const toggleSignup = () => {
-    setShowSignup(!showSignup);
-    setShowLogin(false);
-  };
-  const toggleLogin = () => {
-    setShowLogin(!showLogin);
-    setShowSignup(false);
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
   };
 
-  const handleSubmitSignup = async (event) => {
+  const toggleForm = () => {
+    setFormType(formType === 'login' ? 'signup' : 'login');
+  };
+
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    if (signUpData.password !== signUpData.confirmPassword) {
-      setMessage("Passwords do not match");
+    const endpoint = formType === 'login' ? 'http://localhost:8080/user/login' : 'http://localhost:8080/user/signup';
+
+    if (formType === 'signup' && formData.password !== formData.confirmPassword) {
+      setMessage('Passwords do not match');
       return;
     }
+
     try {
-      const response = await fetch("http://localhost:8080/user/signup", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(signUpData),
-      });
-      if (response.ok) {
-        setMessage("success");
-        toggleLogin();
-      } else {
-        const errorMessage = await response.text();
-        console.log(errorMessage)
-        setMessage(errorMessage);
-      }
-    } catch (error) {
-      console.error("Error occurred while signing up:", error);
-    }
-  }
-  const handleSubmitLogin = async () => {
-    try {
-      const response = await fetch("http://localhost:8080/user/login", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(signInData),
-      });
-      const responseData = await response.json();
-      console.log('Response data:', responseData.message);
-      if (response.ok) {
-        if (responseData.message === "Login successful as client") {
-          setMessage("client");
-//                  handleUserType('client');
-
-           navigate('/ClientMainPage');
-        } else if (responseData.message === "Login successful as admin") {
-          console.log("admin hello")
-
-          setMessage("admin");
-    //              handleUserType('admin');
-
-           navigate('/AdminMainPage');
+      const responseData = await postData(endpoint, formData);
+      if (formType === 'login') {
+        const { message } = responseData;
+        if (message.includes('Login successful')) {
+          const userType = message.includes('client') ? 'client' : 'admin';
+          setMessage(userType);
+          handleUserType(userType);
+          navigate(`/${userType}`);
         } else {
-          setMessage(responseData.message);
+          setMessage(message);
         }
       } else {
-        setMessage(responseData.message); 
-      }}
-  catch (error) {
-      console.error("Error occurred while logging in:", error);
-      setMessage("Error occurred while logging in. Please try again.");
+        setMessage('Signup successful');
+      }
+    } catch (error) {
+      setMessage(error.message || `An error occurred while ${formType === 'login' ? 'logging in' : 'signing up'}`);
     }
   };
-  return (
-        <div className="main-wrapper">
 
+  return (
     <div className="main">
       <input type="checkbox" id="chk" aria-hidden="true" />
-    
-        <form onSubmit={handleSubmitSignup}>
-          <label htmlFor="chk" aria-hidden="true" onClick={toggleSignup}>
-            Sign up
-          </label>
-          <div className="signup">
-          <input
-            type="email"
-            name="email"
-            placeholder="Email"
-            value={signUpData.email}
-            onChange={handleChange}
-            required=""
-          />
-          <input
-            type="text"
-            name="phoneNumber"
-            value={signUpData.phoneNumber}
-            onChange={handleChange}
-            placeholder="Phone Number"
-            required=""
-          />
-          <input
-            type="password"
-            name="password"
-            placeholder="Password"
-            value={signUpData.password}
-            onChange={handleChange}
-            required=""
-          />
-          <input
-            type="password"
-            name="confirmPassword"
-            placeholder="Confirm Password"
-            value={signUpData.confirmPassword}
-            onChange={handleChange}
-            required=""
-          />
-       
-          <button type="submit"> sign up</button>
-          {message && <div className="error-message">{message}</div>}
-    
-      </div>
-          </form>
-      <div className="login">
-          <label htmlFor="chk" aria-hidden="true" onClick={toggleLogin}>
-            Login
-          </label>
-          <input type="text" name="email" value={signInData.email} onChange={handleChange} placeholder="Email" required="" />
-          <input
-            type="password"
-            name="password"
-            placeholder="Password"
-            value={signInData.password}
-            onChange={handleChange}
-            required
-          />
-          <button type="submit" onClick={handleSubmitLogin}>Login</button>
-          {message && <div className="error-message">{message}</div>}
-      </div>
-    </div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="chk" aria-hidden="true" onClick={toggleForm}>
+          {formType === 'login' ? 'Login' : 'Sign up'}
+        </label>
+        <input
+          name="email"
+          placeholder="Email"
+          value={formData.email}
+          onChange={handleChange}
+          required
+        />
+        {formType === 'signup' && (
+          <>
+            <input
+              type="text"
+              name="phoneNumber"
+              value={formData.phoneNumber}
+              onChange={handleChange}
+              placeholder="Phone Number"
+              required
+            />
+            <input
+              type="password"
+              name="confirmPassword"
+              placeholder="Confirm Password"
+              value={formData.confirmPassword}
+              onChange={handleChange}
+              required
+            />
+          </>
+        )}
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={formData.password}
+          onChange={handleChange}
+          required
+        />
+        <button type="submit">{formType === 'login' ? 'Login' : 'Sign up'}</button>
+        {message && <div className="error-message">{message}</div>}
+      </form>
     </div>
   );
 };
+
 export default LoginPage;
