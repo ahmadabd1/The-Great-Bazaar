b420a7b27dd7c961d715b7ed206e5243ab5aa0aa
Emran147 <emran.qedan11@hotmail.com>
2024-03-12 11:23:55 +0200

    cloudinary_items_categories

--- FrontEnd/src/components/admin/AdminMainPage.jsx
+++ FrontEnd/src/components/admin/AdminMainPage.jsx
@@ -1,7 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import CategoryComponent from './CategoryComponent';
-import { Container , Button } from 'react-bootstrap';
+
+
 
 const AdminMainPage = () => {
 
