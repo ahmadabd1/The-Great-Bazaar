b37c894bfc91f58ffa93e217d77acf722ad28186
Ahmad Abdallah <ahmadabdallah174@gmail.com>
2024-03-16 01:34:39 +0200

    update

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -56,7 +56,7 @@ export default function App() {
         <Route path="/item/:id" element={<ItemDetail />} />
       </Routes>
       <Footer />
-      <BottomPage id="contact-us"/>
+      {/* <BottomPage id="contact-us"/> */}
     </>
   );
 }
