e52cda2b8354af107bd7f90320b4a792ae58f35c
lyaleaz <lyale000az@gmail.com>
2024-03-20 00:43:01 +0200

    updates

--- FrontEnd/src/components/admin/AddCategoryModal.jsx
+++ FrontEnd/src/components/admin/AddCategoryModal.jsx
@@ -1,22 +1,18 @@
 import React, { useState } from 'react';
 import '../style/AddCategoryModal.css';
-
 export default function AddCategoryModal({ categories, onSave, onCancel }) {
   const [name, setName] = useState('');
   const [description, setDescription] = useState('');
   const [parentId, setParentId] = useState('');
   const [image, setImage] = useState(null);
-
   const handleSave = () => {
     const formData = new FormData();
     formData.append('name', name);
     formData.append('description', description);
     if (parentId) formData.append('parent_id', parentId);
     if (image) formData.append('image', image);
-    console.log(formData)
     onSave(formData);
   };
-
   return (
     <div className="modal">
       <h2>Add Category</h2>
@@ -47,10 +43,8 @@ export default function AddCategoryModal({ categories, onSave, onCancel }) {
         type="file"
         onChange={(e) => setImage(e.target.files[0])}
       />
-      <div style={{marginBottom:"15px"}}>
       <button onClick={handleSave}>Create</button>
       <button onClick={onCancel}>Cancel</button>
-      </div>
     </div>
   );
 }
--- FrontEnd/src/components/admin/Categories.jsx
+++ FrontEnd/src/components/admin/Categories.jsx
@@ -5,7 +5,6 @@ import usePost from '../customHooks/usePost2';
 import AddCategoryModal from './AddCategoryModal';
 import UpdateCategoryModal from './UpdateCategoryModal';
 import '../style/admin_Categories.css';
-
 export default function Categories() {
   const { data: categories, loading: loadingCategories, error: errorLoading, refetch } = useGet('http://localhost:8080/category/categories/');
   const { deleteItem, isLoading: deleting, error: errorDeleting } = useDelete();
@@ -13,51 +12,43 @@ export default function Categories() {
   const [showAddCategory, setShowAddCategory] = useState(false);
   const [showUpdateCategory, setShowUpdateCategory] = useState(false);
   const [currentCategory, setCurrentCategory] = useState(null);
-
   const handleDelete = async (id) => {
+    console.log(id);
     await deleteItem(`http://localhost:8080/category`, id);
-    refetch(); 
+    refetch();
   };
-
   const handleAddCategoryClick = () => {
     setShowAddCategory(true);
   };
-
   const handleUpdateClick = (category) => {
     setCurrentCategory(category);
     setShowUpdateCategory(true);
   };
-
   const handleSaveNewCategory = async (formData) => {
     try {
-      await postData('http://localhost:8080/category/category/', formData);
+      await postData('http://localhost:8080/category/category/', formData, true);
       setShowAddCategory(false);
       refetch();
     } catch (error) {
       console.error('Error creating category:', error);
     }
   };
-
   const handleUpdateSave = () => {
     setShowUpdateCategory(false);
     refetch();
   };
-
   const handleCancel = () => {
     setShowAddCategory(false);
   };
-
   const handleUpdateCancel = () => {
     setShowUpdateCategory(false);
   };
-
   if (loadingCategories || deleting || posting) return <div>Loading...</div>;
   if (errorLoading || errorDeleting || postError) return <div>Error: {errorLoading?.message || errorDeleting?.message || postError?.message}</div>;
-
   return (
     <div className="categories-container">
       <h1>Categories</h1>
-      <button className='add-new-category' onClick={handleAddCategoryClick}>Add New Category</button>
+      <button  className='add-new-category'onClick={handleAddCategoryClick}>Add New Category</button>
       {showAddCategory && (
         <AddCategoryModal
           categories={categories}
@@ -65,27 +56,18 @@ export default function Categories() {
           onCancel={handleCancel}
         />
       )}
-      <table className='categories-table'>
-        <thead>
-          <tr>
-            <th>Image</th>
-            <th>Name</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {categories && categories.map(category => (
-            <tr key={category._id}>
-              <td><img src={category.image_id} alt={category.name} className="category-image" /></td>
-              <td>{category.name}</td>
-              <td>
-                <button className='update-button' onClick={() => handleUpdateClick(category)}>Update</button>
-                <button className='delete-button' onClick={() => handleDelete(category._id)}>Delete</button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <div className='categories-container2'>
+      <ul>
+        {categories && categories.map(category => (
+          <li key={category._id}>
+            <img src={category.image_id} alt={category.name} style={{ width: '50px', height: '50px' }} />
+            {category.name}
+            <button className='delete-button' onClick={() => handleDelete(category._id)}>Delete</button>
+            <button className='update-button' onClick={() => handleUpdateClick(category)}>Update</button>
+          </li>
+        ))}
+      </ul>
+      </div>
       {showUpdateCategory && (
         <UpdateCategoryModal
           category={currentCategory}
