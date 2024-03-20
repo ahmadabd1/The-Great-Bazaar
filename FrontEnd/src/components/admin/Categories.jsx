50159ebbc31cf6998132c1b0360fe6d5f93b6adc
lyaleaz <lyale000az@gmail.com>
2024-03-20 15:09:25 +0200

    update design

--- FrontEnd/src/components/admin/Categories.jsx
+++ FrontEnd/src/components/admin/Categories.jsx
@@ -5,6 +5,7 @@ import usePost from '../customHooks/usePost2';
 import AddCategoryModal from './AddCategoryModal';
 import UpdateCategoryModal from './UpdateCategoryModal';
 import '../style/admin_Categories.css';
+
 export default function Categories() {
   const { data: categories, loading: loadingCategories, error: errorLoading, refetch } = useGet('http://localhost:8080/category/categories/');
   const { deleteItem, isLoading: deleting, error: errorDeleting } = useDelete();
@@ -12,18 +13,22 @@ export default function Categories() {
   const [showAddCategory, setShowAddCategory] = useState(false);
   const [showUpdateCategory, setShowUpdateCategory] = useState(false);
   const [currentCategory, setCurrentCategory] = useState(null);
+
   const handleDelete = async (id) => {
     console.log(id);
     await deleteItem(`http://localhost:8080/category`, id);
     refetch();
   };
+
   const handleAddCategoryClick = () => {
     setShowAddCategory(true);
   };
+
   const handleUpdateClick = (category) => {
     setCurrentCategory(category);
     setShowUpdateCategory(true);
   };
+
   const handleSaveNewCategory = async (formData) => {
     try {
       await postData('http://localhost:8080/category/category/', formData, true);
@@ -33,22 +38,27 @@ export default function Categories() {
       console.error('Error creating category:', error);
     }
   };
+
   const handleUpdateSave = () => {
     setShowUpdateCategory(false);
     refetch();
   };
+
   const handleCancel = () => {
     setShowAddCategory(false);
   };
+
   const handleUpdateCancel = () => {
     setShowUpdateCategory(false);
   };
+
   if (loadingCategories || deleting || posting) return <div>Loading...</div>;
   if (errorLoading || errorDeleting || postError) return <div>Error: {errorLoading?.message || errorDeleting?.message || postError?.message}</div>;
+
   return (
-    <div className="categories-container">
+    <div className="categoriesclass">
       <h1>Categories</h1>
-      <button  className='add-new-category'onClick={handleAddCategoryClick}>Add New Category</button>
+      <button className='add-new-category' onClick={handleAddCategoryClick}>Add New Category</button>
       {showAddCategory && (
         <AddCategoryModal
           categories={categories}
@@ -56,18 +66,30 @@ export default function Categories() {
           onCancel={handleCancel}
         />
       )}
-      <div className='categories-container2'>
-      <ul>
-        {categories && categories.map(category => (
-          <li key={category._id}>
-            <img src={category.image_id} alt={category.name} style={{ width: '50px', height: '50px' }} />
-            {category.name}
-            <button className='delete-button' onClick={() => handleDelete(category._id)}>Delete</button>
-            <button className='update-button' onClick={() => handleUpdateClick(category)}>Update</button>
-          </li>
-        ))}
-      </ul>
-      </div>
+        <table className="categories-Table">
+          <thead>
+            <tr>
+              <th>Image</th>
+              <th>Name</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {categories && categories.map(category => (
+              <tr key={category._id}>
+                <td><img src={category.image_id} alt={category.name} className="category-image"/></td>
+                <td>{category.name}</td>
+      <td >
+         <div className="buttons">
+                  <button className='update-item-button' onClick={() => handleUpdateClick(category)}>Update</button>
+
+                  <button className='delete-item-button' onClick={() => handleDelete(category._id)}>Delete</button>
+                </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       {showUpdateCategory && (
         <UpdateCategoryModal
           category={currentCategory}
@@ -75,6 +97,7 @@ export default function Categories() {
           onCancel={handleUpdateCancel}
         />
       )}
-    </div>
+            </div>
+
   );
 }
