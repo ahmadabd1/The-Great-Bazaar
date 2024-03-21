ee2517a5745357aa4450a6584f93cb77c5c0989b
TirionFording98 <khalila@mail.tau.ac.il>
2024-03-21 21:21:13 +0200

    full project design + profile + fixed css bugs and frontend bugs

--- FrontEnd/src/components/outside/LoginPage.jsx
+++ FrontEnd/src/components/outside/LoginPage.jsx
@@ -1,11 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import usePost from "../customHooks/usePost";
 import { useNavigate } from "react-router-dom";
 import "../style/LoginPage.css";
 
 function LoginPage() {
-  // State and functions for handling form submission
-  const navigate = useNavigate();
   const [formType, setFormType] = useState("login"); // 'login' or 'signup'
   const [message, setMessage] = useState("");
   const [formData, setFormData] = useState({
@@ -18,6 +16,17 @@ function LoginPage() {
   });
 
   const { postData } = usePost();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // Disable scrolling on mount
+    document.body.style.overflow = "hidden";
+
+    // Re-enable scrolling when component unmounts
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, []);
 
   const handleChange = (event) => {
     const { name, value } = event.target;
@@ -28,7 +37,7 @@ function LoginPage() {
   };
 
   const toggleForm = () => {
-    setFormType(formType === "login" ? "signup" : "login");
+    setFormType((prevType) => (prevType === "login" ? "signup" : "login"));
   };
 
   const handleSubmit = async (event) => {
@@ -74,8 +83,8 @@ function LoginPage() {
   };
 
   return (
-    <div className="mx-auto my-5  mt-16 h-[55%] w-[150vh]  rounded-lg p-20 opacity-85">
-      <div className="overflow-hidden rounded-md bg-gray-950 text-slate-400 shadow-md md:flex">
+    <div className="no-scroll-container mt-[15vh] h-[75vh] w-[150vh] rounded-lg  opacity-85">
+      <div className="overflow-hidden rounded-md  bg-gray-950 text-slate-400 shadow-md md:flex">
         {/* Card Image */}
         <div className="md:w-1/2">
           <img
@@ -86,9 +95,9 @@ function LoginPage() {
         </div>
 
         {/* Card Content */}
-        <div className="p-8 md:w-1/2">
+        <div className="md:w-1/2">
           {/* Logo */}
-          <div className="mb-4 flex items-center">
+          <div className="mb-2 flex items-center">
             <img
               src="./src/assets/BazaarIcon.gif"
               className="w-25  h-28"
@@ -109,7 +118,7 @@ function LoginPage() {
                 value={formData.firstName}
                 onChange={handleChange}
                 required
-                className="text-1xl m-5 mx-auto mt-4 block h-12 w-80 rounded-lg border-gray-300 px-4 py-3 text-black focus:border-orange-500"
+                className="text-1xl m-5 mx-auto mt-1 block h-6 w-80 rounded-lg border-gray-300 px-4 py-3 text-black focus:border-orange-500"
               />
               <input
                 name="lastName"
@@ -117,7 +126,7 @@ function LoginPage() {
                 value={formData.lastName}
                 onChange={handleChange}
                 required
-                className="text-1xl m-5 mx-auto mt-4 block h-12 w-80 rounded-lg border-gray-300 px-4 py-3 text-black focus:border-orange-500"
+                className="text-1xl m-5 mx-auto mb-2 block h-6 w-80 rounded-lg border-gray-300 px-4 py-3 text-black focus:border-orange-500"
               />
             </>
           )}
@@ -128,7 +137,7 @@ function LoginPage() {
             value={formData.email}
             onChange={handleChange}
             required
-            className="text-1xl m-5 mx-auto mt-4 block h-12 w-80 rounded-lg border-gray-300 px-4 py-3 text-black focus:border-orange-500"
+            className="text-1xl m-5 mx-auto mt-4 block h-6 w-80 rounded-lg border-gray-300 px-4 py-3 text-black focus:border-orange-500"
           />
 
           {formType === "signup" && (
@@ -140,7 +149,7 @@ function LoginPage() {
                 onChange={handleChange}
                 placeholder="Phone Number"
                 required
-                className="text-1xl m-5 mx-auto mt-4 block h-12 w-80 rounded-lg border-gray-300 px-4 py-3 text-black focus:border-orange-500"
+                className="text-1xl m-5 mx-auto mt-4 block h-6 w-80 rounded-lg border-gray-300 px-4 py-3 text-black focus:border-orange-500"
               />
               <input
                 type="password"
@@ -149,7 +158,7 @@ function LoginPage() {
                 value={formData.confirmPassword}
                 onChange={handleChange}
                 required
-                className="text-1xl m-5 mx-auto mt-4 block h-12 w-80 rounded-lg border-gray-300 px-4 py-3 text-black focus:border-orange-500"
+                className="text-1xl m-5 mx-auto  block h-6 w-80 rounded-lg border-gray-300   text-black focus:border-orange-500"
               />
             </>
           )}
@@ -161,12 +170,12 @@ function LoginPage() {
             value={formData.password}
             onChange={handleChange}
             required
-            className="text-1xl m-5 mx-auto mt-4 block h-12 w-80 rounded-lg border-gray-300 px-4 py-3 text-black focus:border-orange-500"
+            className="text-1xl m-5 mx-auto mt-4 block h-6 w-80 rounded-lg border-gray-300 px-4 py-3 text-black focus:border-orange-500"
           />
 
           {/* Login Button */}
           <button
-            className="text-1xl mb-5 mt-10 w-full text-pretty rounded-lg bg-gray-800  px-6 py-3 text-center text-xl text-slate-500 hover:bg-cyan-600 hover:text-black"
+            className="text-1xl mb-3 ml-[23vh] mt-8 w-[30vh] text-pretty rounded-lg bg-gray-800  text-center text-xl text-slate-500 hover:bg-cyan-600 hover:text-black hover:text-slate-50"
             type="submit"
             onClick={handleSubmit}
           >
@@ -179,14 +188,14 @@ function LoginPage() {
             <a
               href="#!"
               onClick={toggleForm}
-              className="text-xl font-semibold text-gray-400 hover:text-sky-400"
+              className=" text-xl font-semibold text-gray-400 hover:text-sky-400"
             >
-              {formType === "login" ? "Register here" : "Login"}
+              {formType === "login" ? "Register here" : "<- Login"}
             </a>
           </p>
 
           {/* Links */}
-          <div className="ml-48 flex items-center">
+          <div className="ml-[23vh] flex items-center">
             <a href="#!" className="text-m me-2 text-gray-500">
               Terms of use.
             </a>
